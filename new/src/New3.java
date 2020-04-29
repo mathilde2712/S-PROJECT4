7eab9de2b2c0cadcc0721d2bc31645dd9f556233
Mathilde <Mathilde@Bruger-Pc>
2020-04-29 12:48:31 +0200

    h

--- new/src/New3.java
+++ new/src/New3.java
@@ -3,7 +3,7 @@ public class New3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello World");
-		System.out.println("Bye bye");
+		System.out.println("Bye");
 
 	}
 
